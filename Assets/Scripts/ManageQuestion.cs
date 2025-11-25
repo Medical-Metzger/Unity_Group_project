c2470d3d2539018263224301e24b5778ad5814c1
Laura Hudson <alpenglow.atelier@gmail.com>
2025-11-25 15:29:05 +0000

    Update Quiz for button functions
    Add Confirm and Next buttons, have wrong answers show an X raw image with correct answer with a green checkmark. Add audio clip for correct/incorrect response after click Confirm. Have a Next button automatically replace the Confirm button after the Confirm button and feedback change. Set it up so you cannot change your answer after clicking Confirm.

--- Assets/Scripts/ManageQuestion.cs
+++ Assets/Scripts/ManageQuestion.cs
@@ -37,6 +37,13 @@ public class ManageQuestion:MonoBehaviour
         Toggle selectedToggle = myToggleGroup.ActiveToggles().FirstOrDefault();
         userResponse = selectedToggle.gameObject;
 
+        //Set all toggles as non interactable
+        for (int i = 0; i < myToggleGroup.transform.childCount; i++)
+
+        {
+            myToggleGroup.transform.GetChild(i).GetComponent<Toggle>().interactable = false;
+        }
+
         //Set All toggles as non interactable
         for (int i = 0; i < myToggleGroup.transform.childCount; i++)
         {
