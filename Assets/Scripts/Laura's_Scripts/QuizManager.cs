a19fed25da5698243574fe408a76e5128933ad72
Laura Hudson <alpenglow.atelier@gmail.com>
2025-12-01 15:40:25 +0000

    Add quiz button animation and coroutine for 2 new score panels
    Add % score to count score on Score panel with code, have two new panels to the score panel where one is a - congrats you passed and one is for when the player doesn't pass (fail) using coroutine

--- Assets/Scripts/Laura's_Scripts/QuizManager.cs
+++ Assets/Scripts/Laura's_Scripts/QuizManager.cs
@@ -15,14 +15,37 @@ public class QuizManager : MonoBehaviour
     private GameObject finalPanel;
   
     [SerializeField]
-    private GameObject scoreIndicator;
+    private GameObject scoreIndicator; //the is the textbox for the score
+
+
+    [Header("Score Panels")]
+
+    [SerializeField]
+    private GameObject passPanel; //this is for the people who pass the quiz
+
+    [SerializeField]
+    private GameObject retryPanel; //this is for the people who did not
     
-    public int score = 0;
+    public int score = 0; 
+    
+    /*
+    5 Question Max 
+    Express as fraction out of 5 
+    Must get 3 to pass 
+
+    need to use the if/else statements to check for int i>3 then you will SetActive a UI panel you assign in the scripts as a public GameObject passPanel / redoPanel
+
+    */
+
+    private IEnumerator myLHCoroutine;
    
     // Start is called before the first frame update
     void Start()
     {
     //OnClickNext();
+        /*print("Starting " + Time.time);
+        myLHCoroutine = LHCoroutine(2.0f);
+        StartCoroutine(myLHCoroutine);*/
     }
 
     // Update is called once per frame
@@ -48,12 +71,66 @@ public class QuizManager : MonoBehaviour
         }
         else
         {
+            /*
+                we can either change the ending to only setactive the pass or fail panel 
+                OR 
+                calculate the score and thjen show the pass/fail panel 
+
+            
+            */
             //Show the Score panel
+            
             finalPanel.SetActive(true);
 
             scoreIndicator.GetComponent<TextMeshProUGUI>().text = scoreIndicator.GetComponent<TextMeshProUGUI>().text + score;
 
+            //CheckScore();
+
+            //if statement to check the score int value and if this equals =>3 show the pass panel
+            // if the int score is =<3 show the fail panel 
+
+            /*
+            */
+
             Debug.Log("End");
+        }
+    }
+
+    /*Here is the coroutine functions, add the ButtonClickStartCoroutine method to your buttons in Unity in the inspector */
+
+    public void ButtonClickStartCoroutine()
+    {
+        print("Starting " + Time.time);
+        myLHCoroutine = LHCoroutine(0.01f);
+        StartCoroutine(myLHCoroutine);
+    }
+
+    public IEnumerator LHCoroutine(float waitTime)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            waitTime = 5.0f;
+            print("Waited for " + Time.time);
+            yield return new WaitForSeconds(waitTime);
+            OnClickNext();
+            StopCoroutine(nameof(myLHCoroutine));
+            //yield return null;
+        }
+    }
+
+    public void CheckScore()
+    {
+        
+    if(score < 2)
+        {
+            Debug.Log("Score is" + score);
+            retryPanel.SetActive(true);
+        }
+
+        if(score > 3)
+        {
+            Debug.Log("Score is" + score);
+            passPanel.SetActive(true);
         }
     }
 }
