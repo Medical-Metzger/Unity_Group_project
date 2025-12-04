3026a96e775ae3c8e9b480299af0539e073905ee
Medical-Metzger <karleyprecious@gmail.com>
2025-12-04 10:26:45 +0000

    added steampunk package
    made other changes
    need to fix

--- Assets/Scripts/Precious's scripts/CameraSwitchscript_PK.cs
+++ Assets/Scripts/Precious's scripts/CameraSwitchscript_PK.cs
@@ -11,19 +11,22 @@ public class CameraSwitchscript_PK : MonoBehaviour
     public GameObject camera3;
     public GameObject camera4;
 
-    private int currentTarget;
+    private int currentTarget; //variable declaration
+      // "currentTarget" stores the camera that is active 
 
     void Start()
-    {
+    {//pls make camera 1 the active camera @gamestart
         currentTarget = 1;
         SetCamera(currentTarget);
     }
-    public void SetCamera(int num)
-    {
+    public void SetCamera(int num) ////switch the camera to the number stored in current target 
+    {//assigning numbers to each camera game object 
 
         {
             camera1.SetActive(num == 1);
             camera2.SetActive(num == 2);
+            camera3.SetActive(num == 3);
+            camera4.SetActive(num == 4);
         }
         
     }
